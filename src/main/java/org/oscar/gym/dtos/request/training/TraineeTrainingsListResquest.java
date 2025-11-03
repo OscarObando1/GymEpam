eec4fe5e8c56bab1da4366fc77b780ea0c50ed1b
Oscar <oscar.obandoo@outlook.com>
2025-11-03 13:20:30 -0500

    finished documentation

--- src/main/java/org/oscar/gym/dtos/request/training/TraineeTrainingsListResquest.java
+++ src/main/java/org/oscar/gym/dtos/request/training/TraineeTrainingsListResquest.java
@@ -1,13 +1,17 @@
 package org.oscar.gym.dtos.request.training;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TraineeTrainingsListResquest {
     @NotEmpty
     private String username;
